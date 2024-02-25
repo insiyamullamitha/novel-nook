3a7480c96a0b088248afa8644659be2e17a8ee44
Insiya Mullamitha <insiya.mullamitha@gmail.com>
2024-02-25 12:36:33 +0000

    Filter book titles as user types in search bar

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,20 +1,15 @@
-import { useState } from "react";
 import SearchIcon from "../icons/SearchIcon";
 
 export default function SearchBar({ onSearch }) {
-  const [searchTerm, setSearchTerm] = useState("");
   return (
     <div className="flex flex-row mt-8 h-9 items-center justify-center">
       <input
         type="text"
         className="w-1/2 border-2 bg-white text-black border-black rounded-lg p-2"
         placeholder="Search for a book"
-        onChange={(e) => setSearchTerm(e.target.value)}
+        onChange={(event) => onSearch(event.target.value)}
       />
-      <button
-        onClick={() => onSearch(searchTerm)}
-        className="bg-accent3 text-white rounded-lg p-2 ml-2 hover:bg-accent2 transition-all"
-      >
+      <button className="bg-accent3 text-white rounded-lg p-2 ml-2">
         <SearchIcon />
       </button>
     </div>
